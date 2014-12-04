9facde217f0ad78c72524e1dc4d8b49ee343a1e1
David Ryan <drsaltmills@gmail.com>
2014-12-04 12:54:59 +0000

    fix customer migration

--- db/migrate/20141105171132_change_ids.rb
+++ db/migrate/20141105171132_change_ids.rb
@@ -1,7 +1,6 @@
 class ChangeIds < ActiveRecord::Migration
   def change
   	remove_column :cars, :model_id
-  	remove_column :customers, :customer_id
   	remove_column :parts, :part_id
   	remove_column :employees, :emp_id
   end
